6e6d65f820688d737b742dcc9489a8d4f9c80f30
Dare-Devil <ap.devil48@gmail.com>
2026-01-14 17:52:10 +0530

    python

---
+++ hello.py
@@ -0,0 +1,24 @@
+# age=20
+# price=19.95
+# first_name="Abhay"
+# is_online=False
+# print(age)
+
+# name=input("What's your name? ")
+# print("Hello "+name)
+
+# birth_year=int(input("Enter your birth year: "))
+# age=2020-birth_year
+# print(age)
+# course="python for beginners"
+# print(course.upper())
+# print(course.replace('for', '4'))
+# print(course)
+# print('python' in course)
+# numbers=[1,2,3,4,5]
+# print(len(numbers))
+# numbers.insert(0,2)
+# print(numbers)
+# numbers.remove(3)
+# print(numbers)
+# print(1 in numbers)
